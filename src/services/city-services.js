6273b1193f36e73aad8766dac4b9c50c75b1e54d
Assis-Mohanty <assis.mohanty.98@gmail.com>
2025-02-08 23:05:25 +0530

    city service

---
+++ src/services/city-services.js
@@ -0,0 +1,46 @@
+const {CityRepository}=require("../repository/index")
+
+class CityService{
+    constructor(){
+        this.cityRepository =new CityRepository();
+    }
+
+    async createCity(data){
+        try {
+            const city=await this.cityRepository.createCity(data);
+            return city
+        } catch (error) {
+            console.log("Something went wrong at the service layer")
+            throw {error}
+        }
+    }
+    async deleteCity(data){
+        try {
+            const response =this.cityRepository.deleteCity(data);
+            return true
+        } catch (error) {
+            console.log("Something went wrong at the service layer")
+            throw {error}
+        }
+    }
+
+    async getCity(cityId){
+        try {
+            const city = this.cityRepository.getCity(cityId)
+            return city;
+        } catch (error) {
+            console.log("Something went wrong at the service layer")
+            throw {error}
+        }
+    }
+    async updateCity(cityId,data){
+        try {
+            const city=await this.cityRepository.updateCity(cityId,data);
+            return city
+        } catch (error) {
+            console.log("Something went wrong at the service layer")
+            throw {error}
+        }
+    }
+
+}
